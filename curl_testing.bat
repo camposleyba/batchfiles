a184c5ccef2662727292122178434c3726df13ce
camposleyba <martcamposleyba@gmail.com>
2022-05-05 13:19:55 -0300

    changes to curl bat

--- curl_testing.bat
+++ curl_testing.bat
@@ -1,2 +1,6 @@
 powershell -command curl.exe "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" --output "C:\Users\%USERNAME%\Downloads\python-3.10.4-amd64.exe"
-powershell -command cd C:\Users\%USERNAME%\Downloads\ && start python-3.10.4-amd64.exe /quiet -ArgumentList "InstallAllUsers=1 PrependPath=1 Include_test=0"
+
+powershell -command start-sleep -m 3000
+
+powershell -noexit -command start-process -Filepath "python-3.10.4-amd64.exe" -workingdirectory "C:\Users\%USERNAME%\Downloads" -argumentlist "/quiet, InstallAllUsers=1, PrependPath=1, Include_test=0"
+
