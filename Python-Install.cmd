22ba6c93b97649812fdcd3403ca630947aae6743
camposleyba <martcamposleyba@gmail.com>
2022-05-19 12:41:26 -0300

    changes to python cmd

--- Python-Install.cmd
+++ Python-Install.cmd
@@ -3,7 +3,11 @@ powershell -command curl.exe "https://www.python.org/ftp/python/3.10.4/python-3.
 
 powershell -command start-sleep -m 2000
 
-powershell -noexit -command start-process -Filepath "python-3.10.4-amd64.exe" -workingdirectory "C:\Users\%USERNAME%\Downloads" -argumentlist "/quiet, InstallAllUsers=1, PrependPath=1, Include_test=0"
+powershell -command start-process -Filepath "python-3.10.4-amd64.exe" -workingdirectory "C:\Users\%USERNAME%\Downloads" -argumentlist "/quiet, InstallAllUsers=1, PrependPath=1, Include_test=0"
+
+powershell -command start-sleep -m 2000
+
+powershell -command pip install -r "C:\Users\%USERNAME%\Desktop\requirements.txt"
 
 ECHO Python successfully installed
 REM pip install -r C:\Users\%USERNAME%\Desktop\requirements.txt
