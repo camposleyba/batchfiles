8daa70c5828a2f9a3cd6428d28be72f82e34f700
camposleyba <martcamposleyba@gmail.com>
2022-05-24 18:09:51 -0300

    changes to odbc_setup

--- odbc_setup.cmd
+++ odbc_setup.cmd
@@ -1,8 +1,8 @@
-db2cmd -c -w -i db2 uncatalog node TCP0007
-db2cmd -c -w -i db2 uncatalog odbc data source ICFS
-db2cmd -c -w -i db2 uncatalog db ICFS
-db2cmd -c -w -i db2 uncatalog dcs db ICFS
-db2cmd -c -w -i db2 catalog tcpip node TCP0007 remote iccmvs2.pok.ibm.com server 5500 security ssl
-db2cmd -c -w -i db2 catalog db ICFS as ICFS at node TCP0007 authentication SERVER_ENCRYPT
-db2cmd -c -w -i db2 catalog dcs db ICFS as USIBMVRDP1E parms ',,INTERRUPT_ENABLED,,,,,,'
-db2cmd -c -w -i db2 catalog user odbc data source ICFS
+db2cmd -c -w -i db2 uncatalog node TCP0001
+db2cmd -c -w -i db2 uncatalog odbc data source DP1H
+db2cmd -c -w -i db2 uncatalog db DP1H
+db2cmd -c -w -i db2 uncatalog dcs db DP1H
+db2cmd -c -w -i db2 catalog tcpip node TCP0001 remote usibmvrdp1h.ssiplex.pok.ibm.com server 5520 security ssl
+db2cmd -c -w -i db2 catalog db DP1H as DP1H at node TCP0001 authentication SERVER_ENCRYPT
+db2cmd -c -w -i db2 catalog dcs db DP1H as USIBMVRDP1H parms ',,INTERRUPT_ENABLED,,,,,,'
+db2cmd -c -w -i db2 catalog user odbc data source DP1H
