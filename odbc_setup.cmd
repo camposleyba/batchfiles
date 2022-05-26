dbc92077b285d04b9e6c7e37063c83750b0905d5
camposleyba <martcamposleyba@gmail.com>
2022-05-26 12:59:40 -0300

    changes to setup_cmd

--- odbc_setup.cmd
+++ odbc_setup.cmd
@@ -1,8 +1,8 @@
-db2cmd -c -w -i db2 uncatalog node TCP0001
-db2cmd -c -w -i db2 uncatalog odbc data source DP1H
-db2cmd -c -w -i db2 uncatalog db DP1H
-db2cmd -c -w -i db2 uncatalog dcs db DP1H
-db2cmd -c -w -i db2 catalog tcpip node TCP0001 remote usibmvrdp1h.ssiplex.pok.ibm.com server 5520 security ssl
-db2cmd -c -w -i db2 catalog db DP1H as DP1H at node TCP0001 authentication SERVER_ENCRYPT
-db2cmd -c -w -i db2 catalog dcs db DP1H as USIBMVRDP1H parms ',,INTERRUPT_ENABLED,,,,,,'
-db2cmd -c -w -i db2 catalog user odbc data source DP1H
+db2cmd -c -w -i db2 uncatalog node TCPFIWB1
+db2cmd -c -w -i db2 uncatalog odbc data source EUB1DBM0
+db2cmd -c -w -i db2 uncatalog db EUB1DBM0
+db2cmd -c -w -i db2 uncatalog dcs db EUB1DBM0
+db2cmd -c -w -i db2 catalog tcpip node TCPFIWB1 remote meub1.s390.uk.ibm.com server 447 security ssl
+db2cmd -c -w -i db2 catalog db EUB1DBM0 as EUB1DBM0 at node TCPFIWB1 authentication SERVER_ENCRYPT
+db2cmd -c -w -i db2 catalog dcs db EUB1DBM0 as EUB1DBM0 parms ',,INTERRUPT_ENABLED,,,,,,'
+db2cmd -c -w -i db2 catalog user odbc data source EUB1DBM0
